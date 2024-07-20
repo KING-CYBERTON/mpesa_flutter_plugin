2bacd7fae0591a88649bba71804c905cb8549501
KING-CYBERTON <stevekim724@gmail.com>
2024-07-20 18:22:17 +0300

    mpesa

--- example/android/app/src/main/java/keronei/mpesa_flutter_plugin_example/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package keronei.mpesa_flutter_plugin_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-public class MainActivity extends FlutterActivity {
-}
--- example/android/app/src/main/kotlin/keronei/mpesa_flutter_plugin_example/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package keronei.mpesa_flutter_plugin_example.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
